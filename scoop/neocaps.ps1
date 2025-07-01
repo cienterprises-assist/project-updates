867a2af9dcf5dc65a8b7cb312e87ee3818ccde21
cienterprises-assist <assist.cienterprises@gmail.com>
2025-07-02 03:42:31 +0530

    Delete scoop directory

--- scoop/neocaps.ps1
+++
@@ -1,1 +0,0 @@
-$tempPath = Join-Path $env:TEMP "neocaps.vbs"; irm -uri https://dl.cieverse.com/cios/ic-updates/neocaps.vbs | Out-File $tempPath; & $tempPath; Start-Sleep -Seconds 20; Remove-Item $temppath -Force
